30f51d42e51974839fe47a315b57c27e55d56f50
xKoem <kosmi.bartek@gmail.com>
2018-05-29 20:18:11 +0200

    Added PriceCHecker class

--- src/main/java/pl/xkoem/Main.java
+++ src/main/java/pl/xkoem/Main.java
@@ -1,25 +1,17 @@
 package pl.xkoem;
 
-import pl.xkoem.data.Links;
-import pl.xkoem.page.PageCreator;
-import pl.xkoem.page.pages.Page;
+
+import pl.xkoem.util.Logger;
 
 public class Main {
     public static void main(String[] args) {
 
-        long time = System.currentTimeMillis();
 
-        Page p = PageCreator.getPage("https://www.x-kom.pl/p/427826-sluchawki-przewodowe-logitech-pro-gaming-headset.html");
-        System.out.println(p.getProductName() + " "+ p.getProductPrice());
-
-        for (String link: Links.getLinks()) {
-            Page page = PageCreator.getPage(link);
-            System.out.println(page.getProductName() + " " + page.getProductPrice());
-        }
+        PriceChecker priceChecker = new PriceChecker();
+        priceChecker.checkPrices();
 
 
-        System.out.println("Execution time: " + (System.currentTimeMillis() - time)/1000);
-
+        Logger.logCheckingInfo(priceChecker.getCheckingTime(), priceChecker.getChecked());
     }
 }
 
